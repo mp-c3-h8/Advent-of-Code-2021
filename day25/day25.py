dd560c8326fa500256cb7cbc4c9cf81b637ef59c
mp <no@e.mail>
2026-01-17 02:03:05 +0100

    day25

--- day25/day25.py
+++ day25/day25.py
@@ -3,46 +3,94 @@ from timeit import default_timer as timer
 
 type SeaCucumber = tuple[int, int]  # (y,x)
 type Herd = set[SeaCucumber]
-type Horde = list[Herd]
 
 
-def move(horde: Horde, dimy: int, dimx: int) -> tuple[int, Horde]:
-
+def cucumber_jam(east: Herd, south: Herd, dimy: int, dimx: int) -> tuple[int, Herd, Herd]:
     max_iter = 10**4
 
     for i in range(1, max_iter):
-        movement: bool = False
-
-        for n, (herd, (dy, dx)) in enumerate(zip(horde, ((0, 1), (1, 0)))):
-
-            new: Herd = set()
-            for cuc in herd:
-                moved = ((cuc[0]+dy) % dimy, (cuc[1]+dx) % dimx)
-                if all(moved not in h for h in horde):
-                    new.add(moved)
-                    movement = True
-                else:
-                    new.add(cuc)
-            horde[n] = new
-
+        movement, east, south = move(east, south, dimy, dimx)
         if not movement:
-            return (i, horde)
+            return (i, east, south)
 
     raise ValueError(f"Max iterations {max_iter} reached.")
 
 
-def plot_cucumber(horde: Horde, dimy: int, dimx: int) -> None:
-    import numpy as np
-    import matplotlib.pyplot as plt
+def move(east: Herd, south: Herd, dimy: int, dimx: int) -> tuple[int, Herd, Herd]:
 
+    east_: Herd = set()
+    south_: Herd = set()
+    movement: bool = False
+
+    for cuc in east:
+        moved = (cuc[0], (cuc[1]+1) % dimx)
+        if moved not in east and moved not in south:
+            east_.add(moved)
+            movement = True
+        else:
+            east_.add(cuc)
+    east = east_
+
+    for cuc in south:
+        moved = ((cuc[0]+1) % dimy, cuc[1])
+        if moved not in east and moved not in south:
+            south_.add(moved)
+            movement = True
+        else:
+            south_.add(cuc)
+    south = south_
+
+    return (movement, east, south)
+
+
+def create_matrix(east: Herd, south: Herd, dimy: int, dimx: int):
+    import numpy as np
     X = np.ones((dimy, dimx)) * np.nan
 
+    for cuc in east:
+        X[cuc] = 1
+    for cuc in south:
+        X[cuc] = 5
 
-    for i,herd in enumerate(horde):
-        for cuc in herd:
-            X[cuc] = i
+    return X
+
+
+def plot_cucumber(east: Herd, south: Herd, dimy: int, dimx: int) -> None:
+    import matplotlib.pyplot as plt
+
+    X = create_matrix(east, south, dimy, dimx)
     plt.imshow(X, cmap="tab20b")
     plt.show()
+
+
+def animate_cucumber(east: Herd, south: Herd, dimy: int, dimx: int) -> None:
+    import matplotlib.pyplot as plt
+    from celluloid import Camera
+
+    gcf = plt.gcf()
+    gca = plt.gca()
+    gca.set_axis_off()
+    camera = Camera(gcf)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    out_path = os.path.join(dir_path, "animated_cucumbers.mp4")
+
+    max_iter = 10**4
+
+    for i in range(1, max_iter):
+        movement, east, south = move(east, south, dimy, dimx)
+
+        X = create_matrix(east, south, dimy, dimx)
+        plt.imshow(X, cmap="tab20b")
+        camera.snap()
+
+        if not movement:
+            animation = camera.animate()
+            # animation.save("animated_cucumbers.gif",fps=10)
+            out_path = os.path.join(dir_path, "animated_cucumbers.mp4")
+            animation.save(out_path, fps=30)
+            return
+
+    raise ValueError(f"Max iterations {max_iter} reached.")
 
 
 s = timer()
@@ -56,10 +104,13 @@ east: Herd = {(y, x) for y, row in enumerate(data) for x, c in enumerate(row) if
 south: Herd = {(y, x) for y, row in enumerate(data) for x, c in enumerate(row) if c == "v"}
 dimy, dimx = len(data), len(data[0])
 
-steps, horde = move([east, south], dimy, dimx)
+# animate_cucumber(east, south, dimy, dimx)
+
+steps, east, south = cucumber_jam(east, south, dimy, dimx)
 print("Part 1:", steps)
+
 
 e = timer()
 print("time:", e - s)
 
-plot_cucumber(horde, dimy, dimx)
+plot_cucumber(east, south, dimy, dimx)
